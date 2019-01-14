435f149e39ebb710a0f386bec7320ffc0eda7b9a
Ahlsen95 <eah001@student.uib.no>
2019-01-14 20:12:26 +0100

    Added event listeners for images and game function

--- script.js
+++ script.js
@@ -6,4 +6,25 @@ const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_img = document.getElementById("rock");
 const paper_img = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
+const scissors_img = document.getElementById("scissors");
+
+function game(userInput){
+  console.log("User chose " + userInput);
+}
+
+function main(){
+  rock_img.addEventListener("click", function(){
+    game("rock");
+  });
+
+  paper_img.addEventListener("click", function(){
+    game("paper");
+  });
+
+  scissors_img.addEventListener("click", function(){
+    game("scissors");
+
+  });
+}
+
+main();
