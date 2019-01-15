9b8fe9d2719c38e5e54ebe11d5f9340b0ea33a70
Ahlsen95 <eah001@student.uib.no>
2019-01-15 17:43:34 +0100

    Glow corresponding to win or lose on scoreboard box. Finishing win/lose/draw functions.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const userScore = 0;
-const computerScore = 0;
+var userScore = 0;
+var computerScore = 0;
+const smallUserName = "user".fontsize(3).sub();
+const smallCompName = "comp".fontsize(3).sub();
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.getElementById("result");
 const rock_img = document.getElementById("rock");
 const paper_img = document.getElementById("paper");
 const scissors_img = document.getElementById("scissors");
@@ -15,46 +17,65 @@ function getComputerChoice(){
   return (choices[choice]);
 }
 
+//Adds point to user and green glow on the score board
+function win(user, computer){
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  result_p.innerHTML = `${user.capitalize()}${smallUserName} beats ${computer.capitalize()}${smallCompName}. You win!`;
+  scoreBoard_div.classList.add("green-glow");
+  setTimeout(() => scoreBoard_div.classList.remove("green-glow"),600);
+}
+//Adds point to computer and red glow to the screen board
+function lose(user, computer){
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${user.capitalize()}${smallUserName} loses to ${computer.capitalize()}${smallCompName}. You lose...`;
+  scoreBoard_div.classList.add("red-glow");
+  setTimeout(() => scoreBoard_div.classList.remove("red-glow"),600);
+}
+
+//Adding no points and grey glow to the score board
+function draw(user, computer){
+  result_p.innerHTML = `Both chose ${user.capitalize()}. It's a draw.`;
+  scoreBoard_div.classList.add("gray-glow");
+  setTimeout(() => scoreBoard_div.classList.remove("gray-glow"),600);
+}
+
 //Looks up choices from computer and user and finds out who won.
 function game(userInput){
   const computerChoice = getComputerChoice();
-  console.log(userInput + "  "  + computerChoice);
   switch (userInput  + computerChoice){
     //User wins
     case "rockscissors":
     case "paperrock":
     case "scissorspaper":
-      win();
+      win(userInput, computerChoice);
       break;
     //User loses
     case "rockpaper":
     case "paperscissors":
     case "scissorsrock":
-      lose();
+      lose(userInput, computerChoice);
       break;
     //Draw
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-      draw();
+      draw(userInput, computerChoice);
       break;
   }
 }
 
+// Change fisrt letter of a string to be upper case
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 //main
 function main(){
-  rock_img.addEventListener("click", function(){
-    game("rock");
-  });
-
-  paper_img.addEventListener("click", function(){
-    game("paper");
-  });
-
-  scissors_img.addEventListener("click", function(){
-    game("scissors");
-
-  });
+  rock_img.addEventListener("click", () => game("rock"));
+  paper_img.addEventListener("click", () => game("paper"));
+  scissors_img.addEventListener("click", () =>game("scissors"));
 }
 
 main();
